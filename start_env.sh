8fc519f120299cfee0e289331734c4c980e11e92
SanduIrina <sandu.irina2@gmail.com>
2019-02-13 15:09:17 +0200

    Fix name

--- start_env.sh
+++ start_env.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
-conda install -y pytorch torchvision cudatoolkit=9.0 -c pytorch
+conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
 conda install -y tensorflow-gpu
 git clone https://github.com/maximecb/gym-minigrid.git
 cd gym-minigrid
