385255862b978ba44a63067deddd7a94137ee7bc
andreicnica <andreic.nica@gmail.com>
2019-04-15 12:55:19 +0300

    Fix empty matrix calc

--- agents/ppo_worlds.py
+++ agents/ppo_worlds.py
@@ -635,7 +635,7 @@ class PPOWorlds(TwoValueHeadsBaseGeneral):
                     same = (obs[next_mask] == next_obs[next_mask]).all(1).all(1).all(1)
                     s_act_predict = pred_a[next_mask]
                     s_crt_actions = crt_a[next_mask]
-                    if same.sum() != same.size(0):
+                    if same.sum() != same.size(0) and same.size(0) != 0:
                         agworld_batch_loss_same += loss_m_ag_ap_cross(s_act_predict[same], s_crt_actions[same])
                         agworld_batch_loss_diff += loss_m_ag_ap_cross(s_act_predict[~same], s_crt_actions[~same])
 
@@ -665,7 +665,7 @@ class PPOWorlds(TwoValueHeadsBaseGeneral):
                             same = (obs[mask] == next_obs[mask]).all(1).all(1).all(1)
                             s_act_predict = pred_act_hist[mask]
                             s_crt_actions = action_hist[mask]
-                            if same.sum() != same.size(0):
+                            if same.sum() != same.size(0) and same.size(0) != 0:
                                 agworld_batch_loss_same += loss_m_agstate(s_act_predict[same],
                                                                          s_crt_actions[same])
                                 agworld_batch_loss_diff += loss_m_agstate(s_act_predict[~same],
