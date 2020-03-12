1d4dace0ac677674940e4fd0bd389a2f6eea78ba
andreicnica <andreic.nica@gmail.com>
2020-03-12 14:51:40 -0400

    Wrappers

--- utils/gym_wrappers.py
+++ utils/gym_wrappers.py
@@ -86,31 +86,17 @@ def tensor_out(env):
     return TensorOut(env)
 
 
-class FullyObsWrapperDirDiff(gym.core.ObservationWrapper):
+class FullyObsWrapperDirDiff(FullyObsWrapper):
     """
     Fully observable gridworld using a compact grid encoding
     """
 
-    def __init__(self, env):
-        super().__init__(env)
-
-        self.observation_space.spaces["image"] = spaces.Box(
-            low=0,
-            high=255,
-            shape=(self.env.width, self.env.height, 3),  # number of cells
-            dtype='uint8'
-        )
-
     def observation(self, obs):
         env = self.unwrapped
-        full_grid = env.grid.encode()
-        full_grid[env.agent_pos[0]][env.agent_pos[1]] = np.array([
-            OBJECT_TO_IDX['agent'],
-            COLOR_TO_IDX['red'],
-            env.agent_dir
-        ])
 
-        full_grid += env.agent_dir
+        obs = super().observation(obs)
+
+        full_grid = obs["image"] + env.agent_dir
 
         return {
             'mission': obs['mission'],
